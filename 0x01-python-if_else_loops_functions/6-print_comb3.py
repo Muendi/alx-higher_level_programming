ff8599780e1f1a304a87d35cab9cd36ec6f3a27d
muendi <muendikyalo@gmail.com>
2022-06-09 23:01:07 +0300

    Inventing

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 8):
-    print("{:d}{:d}".format(i, j), end=', ')
+    for j in range(1 + 1, 10):
+        print("{:d}{:d}".format(i, j), end=', ')
 print("{:d}{:d}".format(i + 1, j))
